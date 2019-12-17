d0d8e80b5bddd03c8876109354e8c48f76d6aabd
Leandro Lopes <macuser@iMac.local>
2019-12-16 21:28:15 -0300

    tipagem fraca

---
+++ Seção 3 - JS Fundamentos/tipagemFraca.js
@@ -0,0 +1,10 @@
+let qualquer = 'legal';
+console.log(qualquer);
+console.log(typeof(qualquer));
+
+qualquer = 3.12314;
+console.log(qualquer);
+console.log(typeof(qualquer));
+
+
+// Evite nomes genérico e siglas;
---
+++ Seção 3 - JS Fundamentos/variaveisEConstantes.js
@@ -0,0 +1,12 @@
+// var
+ var a = 3;
+
+
+// let -> crie variaveis usando let
+let b = 4;
+
+
+
+// const
+const c = 5;
+
