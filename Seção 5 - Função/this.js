4111b034861ca03d127eb528289364dc4e20b0a7
Leandro Lopes <leandro.lopes.dev@gmail.com>
2019-12-19 13:16:07 -0300

    revisao

---
+++ Seção 5 - Função/this.js
@@ -0,0 +1,3 @@
+// this pode variar de acordo com a função
+
+// em arrow function não varia
---
+++ Seção 5 - Função/thisEBind1.js
@@ -0,0 +1,16 @@
+// recurso novo do ECMAScript, inserindo uma função dentro de um objeto
+
+const pessoa = {
+    saudacao: 'Bom dia',
+    falar(){
+        console.log(this.saudacao)
+    }
+}
+
+pessoa.falar()
+
+const falar = pessoa.falar
+falar() // conflito entre paradigmas: funcional e OO
+
+const falarDePessoa = pessoa.falar.bind(pessoa)
+falarDePessoa()
